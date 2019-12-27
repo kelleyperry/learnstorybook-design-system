084bc7c0ef96a47fa63b15fcb846ffc7e42778c3
Kelley Perry <kperry@billfire.com>
2019-12-27 13:17:27 -0700

    make button pop

--- src/Button.js
+++ src/Button.js
@@ -33,7 +33,8 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 0;
+  border: 1px solid red;
+  font-size: 20px;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
